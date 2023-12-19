4b87e23d9b91086772df144f8f4acd767cfcb53e
André Bordignon <andre.bordignon@ciss.com.br>
2023-12-19 14:51:21 -0300

    novo bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'custom-background': "url('/assets/background/cs2.png')",
+        'custom-background': "url('/assets/background/CS2.png')",
 
       },
     },
